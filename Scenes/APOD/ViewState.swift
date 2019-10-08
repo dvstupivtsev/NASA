b25c2d74a64559adebb52eb201922347620737f2
dvstupivtsev <dvstupivtsev@gmail.com>
2019-10-08 14:54:16 +0300

    add ViewStateAction file

--- Scenes/APOD/ViewState.swift
+++ Scenes/APOD/ViewState.swift
@@ -18,24 +18,3 @@ struct ViewState {
         let name: String
     }
 }
-
-extension ViewState {
-    typealias Action = (ViewState) -> ViewState
-
-    enum Actions {
-        static func loading() -> Action {
-            return {
-                setup($0) { $0.isLoading = true }
-            }
-        }
-        
-        static func loaded(dates: [String]) -> Action {
-            return {
-                setup($0) {
-                    $0.isLoading = false
-                    $0.dates = dates.map(Day.init(name:))
-                }
-            }
-        }
-    }
-}
---
+++ Scenes/APOD/ViewStateAction.swift
@@ -0,0 +1,26 @@
+//
+//  Created by Stupivtsev Dmitriy on 08.10.2019.
+//
+
+import Foundation
+
+extension ViewState {
+    typealias Action = (ViewState) -> ViewState
+
+    enum Actions {
+        static func loading() -> Action {
+            return {
+                setup($0) { $0.isLoading = true }
+            }
+        }
+        
+        static func loaded(dates: [String]) -> Action {
+            return {
+                setup($0) {
+                    $0.isLoading = false
+                    $0.dates = dates.map(Day.init(name:))
+                }
+            }
+        }
+    }
+}
